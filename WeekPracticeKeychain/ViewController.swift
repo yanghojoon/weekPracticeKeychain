7a41edbdb1292a3264ade6765b4009060f0a1219
yanghojoon <yanghojoon8487@gmail.com>
2022-01-22 19:24:03 +0900

    feat: 일기장 보기 버튼을 누르면 화면이 전환되도록 구현

--- WeekPracticeKeychain/ViewController.swift
+++ WeekPracticeKeychain/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lookDiaryButton: UIButton!
+    @IBOutlet weak var registerNewPasswordButton: UIButton!
+    @IBOutlet weak var changePasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpInsideLookDiaryButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Diary", bundle: nil)
+        let diaryViewController = storyboard.instantiateViewController(withIdentifier: "Diary")
+        
+        present(diaryViewController, animated: true, completion: nil)
+    }
+    
 }
 
